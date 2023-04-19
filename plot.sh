5ebaaf3fc9e168723cb155885abf7eca87c3e221
sam1016yu <miaoyu1016@gmail.com>
2023-04-18 23:34:53 -0400

    modify plot script

--- plot.sh
+++ plot.sh
@@ -8,3 +8,10 @@ python TSEN.py --ce --epoch $i
 python TSEN.py --sim --epoch $i
 python TSEN.py --epoch $i
 done
+
+for i in {1..80}
+do
+python plot_confusion.py --ce --epoch $i
+python plot_confusion.py --sim --epoch $i
+python plot_confusion.py --epoch $i
+done
